b04ae1a83764f410a1a00454182d293aa7bfc679
Mushtariy <mushtariy.nosirova2005@gmail.com>
2024-08-23 01:52:15 +0500

    add changes

--- components/productCard.js
+++ components/productCard.js
@@ -4,11 +4,18 @@ function ProductCard(item) {
 const productCard = document.createElement('div');    
 const imageContainer = document.createElement('div');
 const img = document.createElement('img');
+
 const productDetails = document.createElement('div');
-const title = document.createElement('h2');
+const productInfo = document.createElement('div');
+const title = document.createElement('span');
+const rating = document.createElement('span');
 const priceContainer = document.createElement('div');
-const oldPrice = document.createElement('p');
-const newPrice = document.createElement('p');
+const oldPrice = document.createElement('span');
+const newPrice = document.createElement('span');
+
+
+productInfo.className = 'product-info'
+rating.className = 'rating';
 productCard.className = 'product-card'; 
 imageContainer.className = 'image-container';
 productDetails.className = 'product-details';
@@ -19,29 +26,41 @@ newPrice.className = 'new-price';
 img.src = item.media[0];
 
 
-title.textContent = item.description;
+title.textContent = item.title;
+rating.textContent = `⭐ ${item.rating}`;
 oldPrice.textContent = item.price;
+
+
 if (item.salePercentage) {
     newPrice.textContent = `${
-        item.price - (item.price * item.salePercentage) / 100
+       Math.floor(item.price - (item.price * item.salePercentage) / 100)
     } сум`;
+
+} else if(item.salePercentage == 0){
+    newPrice.textContent = `${item.price} сум`
 }
 
 imageContainer.append(img)
 priceContainer.append(oldPrice, newPrice)
-productDetails.append(title, priceContainer)
+productInfo.append(title, rating)
+productDetails.append(productInfo, priceContainer)
 productCard.append(imageContainer, productDetails)
 
 
-// productCard.onclick = () => {
-//     //  localStorage.setItem('card', item.id)
-//      location.href = '/pages/producrCards/'
-//     const ids = data.goods.map(item => item.id); // Предполагается, что данные находятся в goods
-//     console.log(ids);
-    
-//     localStorage.setItem('goodsIds', JSON.stringify(ids));
-// }
+productCard.onclick = () => {
+     localStorage.setItem('cardId', item.id)
+     location.href = '/pages/productCards/'
+}
 
+
+// let furnitureArr = data.filter((item) => item.type == 'furniture')
+// let PCArr = data.filter((item) => item.type == 'PC')
+// let audioArr = data.filter((item) => item.type == 'audio')
+// let TVArr = data.filter((item) => item.type == 'TV')
+// let kitchenArr = data.filter((item) => item.type == 'kitchen')
+
+
+  
 
 return productCard
 
--- main.js
+++ main.js
@@ -2,36 +2,67 @@ import { footer } from "./components/footer";
 import { header } from "./components/header";
 import { ModalAccount } from "./components/modal";
 import { ProductCard } from "./components/productCard";
-import { Swiper } from "./components/swiper";
+// import { Swiper } from "./components/swiper";
 import { getData, postData } from "./libs/http";
 import { generateToken, reload, validation } from "./libs/utils";
 
 header()
 footer()
 ModalAccount()
-Swiper()
+// Swiper()
 
-
-
-
-
-  // let productCard = getData('goods')
-  // console.log(productCard);
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    }
   
-  
-  // Promise.all({productCard})
-  // .then(({productCard}) => {
-  //   reload(productCard.data.slice(0,5), 'products-box', ProductCard)
-
-  // } )
-  // .catch(error => console.error(error)
-  // )
-
-  getData('goods')
-  .then(res => {
-    reload((res.data), 'products-box', ProductCard);
-    console.log(res);
     
+  });
+
+
+
+
+
+//   let productCard = getData('goods')
+//   console.log(productCard);
+  
+  
+  
+//   Promise.all({productCard})
+//   .then(({productCard}) => {
+//     reload(productCard.data.slice(0,5), 'products-box', ProductCard)
+
+//   } )
+//   .catch(error => console.error(error)
+//   )
+
+getData('goods')
+.then(res => {
+
+    const categorizedGoods = {
+        furniture: res.data.filter(item => item.type === 'furniture'),
+        audio: res.data.filter(item => item.type === 'audio'),
+        TV: res.data.filter(item => item.type === 'TV'),
+        kitchen: res.data.filter(item => item.type === 'kitchen'),
+        PC: res.data.filter(item => item.type === 'PC')
+      };
+      console.log(categorizedGoods);
+
+    reload(categorizedGoods.furniture.slice(0,5), 'furniture', ProductCard)  
+    reload(categorizedGoods.audio.slice(0,5), 'audio', ProductCard)  
+    reload(categorizedGoods.TV.slice(0,5), 'TV', ProductCard)  
+    reload(categorizedGoods.kitchen.slice(0,5), 'kitchen', ProductCard)  
+    reload(categorizedGoods.PC.slice(5,10), 'PC', ProductCard)  
   })
   .catch(error => console.error(error))
 
@@ -41,37 +72,37 @@ Swiper()
 
 
 
-const form = document.forms[0]
+// const form = document.forms[0]
 
-form.onsubmit = (e) => {
-    e.preventDefault()
-    let user = {}
-    let fn = new FormData(form)
-    let token = generateToken()
-    fn.forEach((value, key) => {
-        user[key] = value
-    })
+// form.onsubmit = (e) => {
+//     e.preventDefault()
+//     let user = {}
+//     let fn = new FormData(form)
+//     let token = generateToken()
+//     fn.forEach((value, key) => {
+//         user[key] = value
+//     })
     
 
-    let phone = fn.get('phone')
+//     let phone = fn.get('phone')
     
    
-getData(`accounts?phone=${phone}`)
-.then(res => {
-    console.log(res.data);
+// getData(`accounts?phone=${phone}`)
+// .then(res => {
+//     console.log(res.data);
     
-        if(validation()){
+//         if(validation()){
            
-            postData('accounts', {token, ...user})
-                .then((res) => {
-                    localStorage.setItem("token", res.data.token)
-                    localStorage.setItem("userId", res.data.id)
-                    window.location.replace('/')
-                })
-                .catch((error) => console.log(error))
-        }
+//             postData('accounts', {token, ...user})
+//                 .then((res) => {
+//                     localStorage.setItem("token", res.data.token)
+//                     localStorage.setItem("userId", res.data.id)
+//                     window.location.replace('/')
+//                 })
+//                 .catch((error) => console.log(error))
+//         }
     
-})
-.catch(error => console.log(error))
-}
+// })
+// .catch(error => console.log(error))
+// }
 
